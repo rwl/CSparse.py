a018cd96f6c2dc4a3c9555f866c0bea596815247
rwl <r.w.lincoln@gmail.com>
2012-06-02 21:08:49 +0100

    Adding readme, license and setup files.

---
+++ readme.rst
@@ -0,0 +1,22 @@
+CSparse.py: a Concise Sparse matrix Python module
+=================================================
+
+Copyright (C) 2006-2011, Timothy A. Davis
+
+Copyright (C) 2012, Richard Lincoln
+
+http://www.cise.ufl.edu/research/sparse/CSparse/
+
+CSparse.py is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+CSparse.py is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this Module; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
---
+++ setup.py
@@ -0,0 +1,25 @@
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+cwd = abspath(dirname(__file__))
+readme = open(join(cwd, 'readme.rst'))
+kwds = {'long_description': readme.read()}
+readme.close()
+
+setup(name='CSparse.py',
+      version='1.0.0',
+      description='CSparse.py: a Concise Sparse matrix Python module',
+      author='Richard Lincoln',
+      author_email='r.w.lincoln@gmail.com',
+      url='http://www.cise.ufl.edu/research/sparse/CSparse/',
+      install_requires=['numpy'],
+      classifiers=['Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Mathematics'],
+      include_package_data=True,
+      py_modules=['csparse', 'csparse_test'],
+      test_suite='csparse_test.main',
+      zip_safe=True,
+      **kwds)
